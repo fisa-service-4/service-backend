db7c545ba3b827ef7b09b69c8cc57d6d82424e19
yujung <kimyj4017@gmail.com>
2026-05-29 14:41:46 +0900

    fix(stock): 관심종목 등록 시 중복 에러코드 수정 (#44)

--- src/main/java/com/service/global/exception/ErrorCode.java
+++ src/main/java/com/service/global/exception/ErrorCode.java
@@ -69,7 +69,10 @@ public enum ErrorCode {
 
   // STOCK
   STOCK_001(HttpStatus.NOT_FOUND, "STOCK_001", "증권 계좌를 찾을 수 없습니다."),
-  STOCK_002(HttpStatus.CONFLICT, "STOCK_002", "이미 등록된 관심종목입니다.");
+
+  // FAVORITE
+  FAVORITE_001(HttpStatus.NOT_FOUND, "FAVORITE_001", "관심종목 없음"),
+  FAVORITE_002(HttpStatus.CONFLICT, "FAVORITE_002", "이미 등록된 관심종목입니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
