b109ff62557f3a075d500c69303d6bfeb4d0dc08
//G0G// <80445119+ktx06155@users.noreply.github.com>
2026-06-04 12:24:47 +0900

    Add validation for payday field in request DTO

--- src/main/java/com/service/domain/virtualsalary/dto/request/VirtualSalarySettingRequest.java
+++ src/main/java/com/service/domain/virtualsalary/dto/request/VirtualSalarySettingRequest.java
@@ -20,7 +20,7 @@ public class VirtualSalarySettingRequest {
     @DecimalMin(value = "0.01", message = "가상월급 금액은 0보다 커야 합니다.")
     private BigDecimal targetSalary;
 
-    //  @NotNull(message = "월급일은 필수입니다.")
+    @NotNull(message = "월급일은 필수입니다.")
     @Min(value = 1, message = "월급일은 1 이상이어야 합니다.")
     @Max(value = 31, message = "월급일은 31 이하이어야 합니다.")
     private Integer payday;
