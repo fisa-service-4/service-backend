0324d4cbd66f222f89e4221cb6d5a29bc70b3168
yujung <kimyj4017@gmail.com>
2026-06-05 14:17:08 +0900

    [fix/stock] 증권 계좌 조회 x-firebase-uid 헤더 의존성 제거 (#67)
    * fix(stock): StockController 증권 계좌 조회x-firebase-uid 헤더 의존성 제거
    
    * fix(stock): stock_check.py에 하드코딩 제거
    
    * refactor(stock): StockController UserRepository 의존성을 Service로 이동

--- src/main/java/com/service/domain/stock/service/StockService.java
+++ src/main/java/com/service/domain/stock/service/StockService.java
@@ -2,7 +2,10 @@ package com.service.domain.stock.service;
 
 import com.service.domain.stock.dto.response.CashBalanceResponse;
 import com.service.domain.stock.dto.response.StockAccountsResponse;
+import com.service.domain.user.repository.UserRepository;
 import com.service.global.client.TransactionServerClient;
+import com.service.global.exception.BusinessException;
+import com.service.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +14,16 @@ import org.springframework.stereotype.Service;
 public class StockService {
 
   private final TransactionServerClient transactionServerClient;
+  private final UserRepository userRepository;
 
   public CashBalanceResponse getCashBalance(Long accountId) {
     return CashBalanceResponse.from(transactionServerClient.getCashBalance(accountId));
   }
 
-  public StockAccountsResponse getStockAccounts(String firebaseUid) {
+  public StockAccountsResponse getStockAccounts(Long userId) {
+    String firebaseUid = userRepository.findById(userId)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_001))
+        .getFirebaseUid();
     return StockAccountsResponse.from(transactionServerClient.getStockAccounts(firebaseUid));
   }
 }
